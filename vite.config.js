e7e97fca0ddbbd151ed60ff214305ae5eeefed43
Сергей Бабич <ssscgdsl@gmail.com>
2025-07-29 08:49:21 +0300

    fix: clean config and build success locally

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,5 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  root: '.',                     // Явно вказуємо корінь проєкту
   plugins: [react()],
-  build: {
-    outDir: 'dist',              // Папка для фінального білду
-    rollupOptions: {
-      input: './index.html'      // ← Ось ключовий рядок
-    }
-  }
 });
