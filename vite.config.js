f4bfd0b71673840e4c2ee1984a9e706214e59447
Сергей Бабич <ssscgdsl@gmail.com>
2025-07-29 08:46:07 +0300

    fix: specify index.html as entry in vite.config

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,12 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()]
+  root: '.',                     // Явно вказуємо корінь проєкту
+  plugins: [react()],
+  build: {
+    outDir: 'dist',              // Папка для фінального білду
+    rollupOptions: {
+      input: './index.html'      // ← Ось ключовий рядок
+    }
+  }
 });
